492df95a99eaead0104498e6a296aac57a02c725
Leon Luithlen <leontimnaluithlen@gmail.com>
2024-09-30 13:16:45 +0200

    Use cosine distance

--- scripts/extract_codas.py
+++ scripts/extract_codas.py
@@ -3,7 +3,7 @@ import string
 
 import numpy as np
 import pandas as pd
-from sklearn.metrics.pairwise import manhattan_distances
+from sklearn.metrics.pairwise import manhattan_distances, cosine_distances
 
 rhythm = {
     "1+1+3": 5,
@@ -110,7 +110,7 @@ def coda_distances(sequence, means, only_equal=True):
             sequence_normalized = np.cumsum(sequence_ / sequence_.sum())[: len(mean)]
             n_equal_one = np.sum(np.abs(sequence_normalized - 1.0) < 1e-10)
             if (n_equal_one <= 1 and not only_equal) or n_equal_one == 1:
-                distance[coda] = manhattan_distances(
+                distance[coda] = cosine_distances(
                     sequence_normalized.reshape(1, -1), mean.reshape(1, -1)
                 )[0][0]
 
@@ -309,4 +309,4 @@ if __name__ == "__main__":
     )
 
     new_data["Coda"] = [int(v) if not pd.isnull(v) else -1 for v in new_data["Coda"]]
-    new_data.to_csv("data/sperm-whale-dialogues-codas.csv", index=False)
+    new_data.to_csv("data/sperm-whale-dialogues-codas-cosine.csv", index=False)
